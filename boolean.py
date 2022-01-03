e8fe3dc8a8751ef28b7a58777dcbbcd15488f3f6
Juan <juancruzeiriz@gmail.com>
2022-01-03 19:14:15 -0300

    defino funcion que devuelve true si los 3 params son true

---
+++ boolean.py
@@ -0,0 +1,6 @@
+# return true if the three parameters are TRUE, mediante operadores logicos AND 
+def triple_and(x,y,z):
+    # return True if x and y and z else False
+    return x and y and z
+
+print(triple_and(1,2,3))
